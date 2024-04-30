d75697923fa06310003a61c489dd2ab15f95afce
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-30 05:01:37 +0300

    Add filter feature

--- src/script.js
+++ src/script.js
@@ -3,36 +3,39 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     // Show loading status on the button
-    const searchButton = document.querySelector('#search_button')
-    searchButton.value = 'Loading...'
+    const searchButton = document.querySelector("#search_button");
+    searchButton.value = "Loading...";
     // Call OpenLibrary API
     const userInput = e.target.search_item.value;
     fetch(`https://openlibrary.org/search.json?q=${userInput}`)
       .then((res) => res.json())
       .then((data) => {
-        const searchCount = document.querySelector('#search-count')
+        const searchCount = document.querySelector("#search-count");
         // Display the number of books found
-        searchCount.textContent = data.numFound
+        searchCount.textContent = data.numFound;
         // Unhide the search results section
         const searchResultsSection = document.querySelector("#search-results");
         searchResultsSection.classList.remove("hidden");
         // Empty the books container
         const booksContainer = document.querySelector("#books-found");
-        booksContainer.innerHTML = ''
+        booksContainer.innerHTML = "";
         // Change status of the button
-        searchButton.value = 'Search'
+        searchButton.value = "Search";
         // Scroll to the search results section
         searchResultsSection.scrollIntoView({ behavior: "smooth" });
 
         data.docs.forEach((book) => {
           const bookCard = document.createElement("li");
           bookCard.classList.add("book-card");
-          const publishYears = book.publish_year
+          const publishYears = book.publish_year;
           bookCard.innerHTML = `
                 <div id="cover-container">
                     <img
-                        src=${ // Show cover not found image if the book has no cover id
-                          book.cover_i ? `http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg` : './assets/cover-not-found.png'
+                        src=${
+                          // Show cover not found image if the book has no cover id
+                          book.cover_i
+                            ? `http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+                            : "./assets/cover-not-found.png"
                         }
                         alt="book cover"
                         id="book-cover"
@@ -41,26 +44,78 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div id="book-details">
                     <p>
                     <span id="book-title">${book.title}</span> by
-                    <span id="author">${  // If author_name has no value show Unknown
+                    <span id="author">${
+                      // If author_name has no value show Unknown
                       // if author_name has multiple authors, convert the array to strings seperated by commas else show the single author
-                      book.author_name ? typeof book.author_name === 'object' ? book.author_name.join(", "): book.author_name :"Unknown" 
+                      book.author_name
+                        ? typeof book.author_name === "object"
+                          ? book.author_name.join(", ")
+                          : book.author_name
+                        : "Unknown"
                     }</span>
                     </p>
 
                     <p>
                     <span id="pages">${
-                      book.number_of_pages_median ? book.number_of_pages_median : "Unknown"
+                      book.number_of_pages_median
+                        ? book.number_of_pages_median
+                        : "Unknown"
                     }</span> pages .
-                    <span id="pub-year">${
-                      typeof book.publish_year === 'object' ? book.publish_year[publishYears.length - 1] : book.publish_year
+                    <span id="pub_year">${
+                      typeof book.publish_year === "object"
+                        ? book.publish_year[publishYears.length - 1]
+                        : book.publish_year
                     }</span>
                     </p>
                     <p id="overview">${
-                      book.first_sentence ? book.first_sentence + '..' : "Excerpt not available"
+                      book.first_sentence
+                        ? book.first_sentence + ".."
+                        : "Excerpt not available"
                     }</p>
                 </div>
                 `;
           booksContainer.appendChild(bookCard);
+        });
+        // Add Filter feature
+        const filterDropDown = document.querySelector("#filter");
+
+        // Define filterByPubYear function
+        const filterByPubYear = (pubYear) => {
+          const bookCards = document.querySelectorAll(".book-card");
+          const bookCardsArray = Array.from(bookCards);
+          bookCardsArray.forEach((bookCard) => {
+            const currentBookPubYearElement = bookCard.querySelector('#pub_year');
+            const currentBookPubYear = currentBookPubYearElement.textContent;
+            if (parseInt(currentBookPubYear) <= pubYear) {
+              bookCard.classList.add("hidden");
+            }
+          });
+        };
+        // Add change event listener to the dropdown
+        filterDropDown.addEventListener("change", (e) => {
+          let pubYear;
+          switch (e.target.value) {
+            case "2015":
+              pubYear = parseInt("2015");
+              filterByPubYear(pubYear);
+              break;
+            case "2010":
+              pubYear = parseInt("2010");
+              filterByPubYear(pubYear);
+              break;
+            case "2005":
+              pubYear = parseInt("2005");
+              filterByPubYear(pubYear);
+              break;
+            case "2000":
+              pubYear = parseInt("2000");
+              filterByPubYear(pubYear);
+              break;
+            case "1990":
+              pubYear = parseInt("1990");
+              filterByPubYear(pubYear);
+              break;
+          }
         });
       })
       .catch((error) => console.log(error));
